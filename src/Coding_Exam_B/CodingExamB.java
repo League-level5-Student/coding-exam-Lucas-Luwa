c93337327143baf6ba052068810f2595707f117f
League Student <league@iMac-5.attlocal.net>
2019-07-26 16:15:00 -0700

    Started Part 2
    Begin 6/26/19

--- src/Coding_Exam_B/CodingExamB.java
+++ src/Coding_Exam_B/CodingExamB.java
@@ -8,35 +8,58 @@ import java.io.IOException;
 
 public class CodingExamB {
 	/*
-	 * This is a logging program for collecting TODO comments in a program. The program will scan
-	 * Through all the files in the Coding_Exam_B/classes package, and collect all the comments that start
-	 * with //TODO: and write them to their own file. See the TODO_Log_example.txt file for an idea of what 
-	 * the final file output will look like.
+	 * This is a logging program for collecting TODO comments in a program. The
+	 * program will scan Through all the files in the Coding_Exam_B/classes package,
+	 * and collect all the comments that start with //TODO: and write them to their
+	 * own file. See the TODO_Log_example.txt file for an idea of what the final
+	 * file output will look like.
 	 */
-	
-	
 	public static String getLoggingInfo(String fileName) {
 		/*
-		 * 1. Complete the getLoggingInfoMethod.
-		 *    The method takes in a String for a file and returns a String. 
-		 *    The method will open the file a read through it. It will then 
-		 *    take all the comments that begin with //TODO: and combine them 
-		 *    into one large String. The string will also state the file name and
-		 *    the line number for where each TODO was found. 
-		*/
-		
-		return "";
+		 * 1. Complete the getLoggingInfoMethod. The method takes in a String for a file
+		 * and returns a String. The method will open the file a read through it. It
+		 * will then take all the comments that begin with //TODO: and combine them into
+		 * one large String. The string will also state the file name and the line
+		 * number for where each TODO was found.
+		 */
+		String holder = "";
+		try {
+			// part 2 works, part 1 needs work
+			BufferedReader BR = new BufferedReader(new FileReader(fileName));
+			String line = "";
+			while(!line.equals(null)) {
+
+			line = BR.readLine();
+			System.out.println(line);
+			if (line.contains("//TODO:")) {
+				holder += line + "\n";
+			}
+			}
+			BR.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return holder;
 	}
-	
+
 	public static void main(String[] args) {
 		String finalLogString = getLoggingInfo("src/Coding_Exam_B/classes/Camera.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracedImageViewer.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracer.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/Vector3.java");
-		
 		/*
-		 * 2. Write the finalLogString to a file called TODO_Log.txt. The file should match TODO_Log_example.txt. 
+		 * 2. Write the finalLogString to a file called TODO_Log.txt. The file should
+		 * match TODO_Log_example.txt.
 		 */
-
+		try {
+			FileWriter FW = new FileWriter("src/Coding_Exam_B/TODO_Log.txt");
+			System.out.println(finalLogString);
+			FW.write(finalLogString);
+			FW.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
